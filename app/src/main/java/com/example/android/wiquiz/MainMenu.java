3f0b7c2c30715987573e9f4555c247d9df5cc131
nub <nubishjasrotia26@gmail.com>
2016-03-30 05:04:43 +0530

    after merge

--- app/src/main/java/com/example/android/wiquiz/MainMenu.java
+++ app/src/main/java/com/example/android/wiquiz/MainMenu.java
@@ -1,5 +1,7 @@
 package com.example.android.wiquiz;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,14 +10,14 @@ import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity implements View.OnClickListener {
 
-    Button playButton,addCategoryButton,shareCategoryButton,editProfileButton,quitButton;
+    Button playButton, addCategoryButton, shareCategoryButton, editProfileButton, quitButton;
 
     private void instantiate() {
-        playButton= (Button) findViewById(R.id.buttonPlay);
-        addCategoryButton= (Button) findViewById(R.id.buttonAddCategory);
-        shareCategoryButton= (Button) findViewById(R.id.buttonShareCategory);
-        editProfileButton= (Button) findViewById(R.id.buttonEditProfile);
-        quitButton= (Button) findViewById(R.id.buttonQuit);
+        playButton = (Button) findViewById(R.id.buttonPlay);
+        addCategoryButton = (Button) findViewById(R.id.buttonAddCategory);
+        shareCategoryButton = (Button) findViewById(R.id.buttonShareCategory);
+        editProfileButton = (Button) findViewById(R.id.buttonEditProfile);
+        quitButton = (Button) findViewById(R.id.buttonQuit);
     }
 
     @Override
@@ -25,14 +27,51 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
 
         instantiate();
         playButton.setOnClickListener(this);
+        quitButton.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.buttonPlay)
-        {
-            startActivity(new Intent(this,QuizMode.class));
+        switch (v.getId()) {
+            case R.id.buttonPlay:
+                startActivity(new Intent(this, QuizMode.class));
+                break;
+
+            case R.id.buttonQuit:
+                dialogBox();
+                break;
+
+
         }
+    }
+
+    public void dialogBox() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.quit_dialog_message);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+                int pid = android.os.Process.myPid();
+                android.os.Process.killProcess(pid);
+            }
+        });
+
+        builder.setNegativeButton(R.string.No, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
     }
 }
